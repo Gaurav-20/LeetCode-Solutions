bd1cd86fc407c855642a1b5f3e11ea3d54bfb77e
Gaurav-20 <39230629+Gaurav-20@users.noreply.github.com>
2022-04-21 11:00:26 +0530

    Time: 197 ms (32.40%), Space: 49.8 MB (33.76%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,20 +1,40 @@
 class MyHashSet {
 public:
-    vector<bool> table;
+    int prime;
+    vector<list<int>> table;
+    
+    int hash(int key) {
+        return key % prime;
+    }
+    
+    list<int>::iterator search(int key) {
+		int h = hash(key);
+		return find(table[h].begin(), table[h].end(), key);
+	}
+    
     MyHashSet() {
-        table.resize(1e6 + 1, false);
+        prime = 10007;
+        table.resize(prime);
     }
     
     void add(int key) {
-        table[key] = true;
+        int h = hash(key);
+		if (!contains(key)) {
+			table[h].push_back(key);
+        }
     }
     
     void remove(int key) {
-        table[key] = false;
+        int h = hash(key);
+		auto it = search(key);
+		if (it != table[h].end()) {
+			table[h].erase(it);
+        }
     }
     
     bool contains(int key) {
-        return table[key];
+        int h = hash(key);
+		return search(key) != table[h].end();
     }
 };
 
