9518fa16d5c646a8f116bf306593a6812823819e
Gaurav-20 <39230629+Gaurav-20@users.noreply.github.com>
2022-04-23 11:22:39 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (29.61%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,20 +1,31 @@
 string TINYURL = "https://tinyurl.com/";
 int length = TINYURL.size();
 
+string toString(long long num) {
+    string res = "";
+    while (num > 0) {
+        res += (num % 10) + '0';
+        num /= 10;
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
+
 class Solution {
 public:
-    int it = 0;
-    unordered_map<int, string> code;
-
+    unordered_map<string, string> code;
+    hash<string> hasher;
+    
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        code[++it] = longUrl;
-        return TINYURL + to_string(it);
+        string hash = toString(hasher(longUrl));
+        code[hash] = longUrl;
+        return TINYURL + hash;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return code[stoi(shortUrl.substr(length))];
+        return code[shortUrl.substr(length)];
     }
 };
 
