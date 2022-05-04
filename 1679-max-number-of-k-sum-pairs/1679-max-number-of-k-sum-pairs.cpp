25c6d548c364c80f541be5c4baaa84e0563adfa8
Gaurav-20 <39230629+Gaurav-20@users.noreply.github.com>
2022-05-04 11:17:39 +0530

    Time: 160 ms (73.87%), Space: 58.2 MB (62.65%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
         int operations = 0;
-        map<int, int> hash;
-        for (int& n : nums) {
-            hash[n]++;
-        }
-        for (auto it : hash) {
-            int curr = it.first;
-            int comp = k - curr;
-            if (curr == comp) {
-                if (hash[curr] > 1) {
-                    operations += hash[curr] / 2;
-                    hash[curr] = 0;
-                }
+        sort(nums.begin(), nums.end());
+        int i = 0, j = nums.size() - 1;
+        while (i < j) {
+            int sum = nums[i] + nums[j];
+            if (sum == k) {
+                operations++;
+                i++;
+                j--;
+            } else if (sum > k) {
+                j--;
             } else {
-                if (hash[curr] > 0 && hash[comp] > 0) {
-                    operations += min(hash[curr], hash[comp]);
-                    hash[curr] = hash[comp] = 0;
-                }
+                i++;
             }
         }
         return operations;
