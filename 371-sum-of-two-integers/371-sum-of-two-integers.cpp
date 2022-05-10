f46035860fad93cef12be0ea4fbc3f3588e635bf
Gaurav-20 <39230629+Gaurav-20@users.noreply.github.com>
2022-05-10 19:47:06 +0530

    Time: 0 ms (100.00%), Space: 6 MB (26.28%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
+    void solve(int a, int b, int i, int& sum, int& carry) {
+        if (carry) {
+            if (a == b) {
+                if (a == 0) {
+                    sum |= (1 << i);
+                    carry = 0;
+                } else {
+                    sum |= (1 << i);
+                }
+            }
+        } else {
+            if (a == b) {
+                if (a == 1) {
+                    carry = 1;
+                }
+            } else {
+                sum |= (1 << i);
+            }
+        }
+    }
+    
     int getSum(int a, int b) {
-        return a + b;
+        int sum = 0;
+        int carry = 0;
+        for (int i = 0; i < 32; i++) {
+            int aBit = (a >> i) & 1;
+            int bBit = (b >> i) & 1;
+            solve(aBit, bBit, i, sum, carry);
+        }
+        return sum;
     }
 };
