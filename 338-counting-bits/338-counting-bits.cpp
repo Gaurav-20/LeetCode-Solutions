2469b781fb0663cbd64d49a177131c692c247825
Gaurav-20 <39230629+Gaurav-20@users.noreply.github.com>
2022-05-10 19:50:32 +0530

    Time: 7 ms (62.25%), Space: 7.8 MB (74.33%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -3,7 +3,9 @@ public:
     vector<int> countBits(int n) {
         vector<int> res(n + 1, 0);
         for (int i = 1; i <= n; i++) {
-            res[i] = res[i >> 1] + (i % 2);
+            int quotient = i >> 1;
+            int remainder = i % 2;
+            res[i] = res[quotient] + remainder;
         }
         return res;
     }
