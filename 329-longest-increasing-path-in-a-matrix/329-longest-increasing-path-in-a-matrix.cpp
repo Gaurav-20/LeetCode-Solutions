626c9034ff2e870d61e5e2ae94c7048bd626b8b5
Gaurav-20 <39230629+Gaurav-20@users.noreply.github.com>
2022-05-19 19:40:45 +0530

    Time: 43 ms (91.89%), Space: 16.1 MB (55.62%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,28 +1,24 @@
+vector<int> dir = { 0, 1, 0, -1, 0 };
+
 class Solution {
 public:
     int rows, cols;
-    
-    int solve(vector<vector<int>>& matrix, int r, int c, vector<vector<int>>& dp) {
+
+    int solve(vector<vector<int>>& mat, int r, int c, vector<vector<int>>& dp) {
         if (r < 0 || r >= rows || c < 0 || c >= cols) {
             return 0;
         }
         if (dp[r][c] != -1) {
             return dp[r][c];
         }
-        int up = 0, down = 0, left = 0, right = 0;
-        if (r > 0 && matrix[r - 1][c] > matrix[r][c]) {
-            up = 1 + solve(matrix, r - 1, c, dp);
+        int res = 0;
+        for (int i = 0; i < 4; i++) {
+            int nr = r + dir[i], nc = c + dir[i + 1];
+            if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && mat[nr][nc] > mat[r][c]) {
+                res = max(res, 1 + solve(mat, nr, nc, dp));
+            }
         }
-        if (r < rows - 1 && matrix[r + 1][c] > matrix[r][c]) {
-            down = 1 + solve(matrix, r + 1, c, dp);
-        }
-        if (c > 0 && matrix[r][c - 1] > matrix[r][c]) {
-            left = 1 + solve(matrix, r, c - 1, dp);
-        }
-        if (c < cols - 1 && matrix[r][c + 1] > matrix[r][c]) {
-            right = 1 + solve(matrix, r, c + 1, dp);
-        }
-        return dp[r][c] = max({ up, down, left, right, 1 });
+        return dp[r][c] = max(res, 1);
     }
     
     int longestIncreasingPath(vector<vector<int>>& matrix) {
