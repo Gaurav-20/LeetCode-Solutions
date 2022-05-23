a9077889272452edbe5689e14c31b9e73dbec87b
Gaurav-20 <39230629+Gaurav-20@users.noreply.github.com>
2022-05-23 09:16:14 +0530

    Time: 385 ms (50.52%), Space: 35.1 MB (54.85%) - LeetHub

---
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -0,0 +1,39 @@
+int dp[605][105][105];
+
+class Solution {
+public:
+    int getNumberOfZeroes(string str) {
+        int count = 0;
+        for (char c : str) {
+            if (c == '0') {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    int solve(vector<string>& strs, int it, int len, int m, int n) {
+        if (it >= len) {
+            return 0;
+        }
+        if (dp[it][m][n] != -1) {
+            return dp[it][m][n];
+        }
+        int numZeroes = getNumberOfZeroes(strs[it]);
+        int numOnes = strs[it].size() - numZeroes;
+        dp[it][m][n] = solve(strs, it + 1, len, m, n);
+        if (m >= numZeroes && n >= numOnes) {
+            dp[it][m][n] = max(
+                dp[it][m][n], 
+                1 + solve(strs, it + 1, len, m - numZeroes, n - numOnes)
+            );
+        }
+        return dp[it][m][n];
+    }
+    
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        memset(dp, -1, sizeof(dp));
+        int len = strs.size();
+        return solve(strs, 0, len, m, n);
+    }
+};
