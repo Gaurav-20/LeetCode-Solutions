cd44f22fd507ad27d6c0d9aa0ed051ee70c16e1d
Gaurav-20 <39230629+Gaurav-20@users.noreply.github.com>
2022-05-31 10:36:57 +0530

    Time: 502 ms (47.48%), Space: 71.6 MB (51.89%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
-    long long toInt(string s, int k) {
-        long long res = 0;
-        for (int i = k - 1; i >= 0; i--) {
-            res = (res * 2) + (s[i] - '0');
-        }
-        return res;
-    }
-    
     bool hasAllCodes(string s, int k) {
         int len = s.size();
-        if (len < (1 << k)) {
+        if (len < k) {
             return false;
         }
-        string temp = "";
-        for (int i = 0; i < k; i++) {
-            temp += s[i];
+        unordered_set<string> st;
+        for (int i = 0; i <= len - k; i++) {
+            st.insert(s.substr(i, k));
         }
-        unordered_set<long long> st;
-        st.insert(toInt(temp, k));
-        for (int i = k; i < len; i++) {
-            temp.erase(0, 1);
-            temp += s[i];
-            st.insert(toInt(temp, k));
-        }
-        return st.size() == (1LL << k);
+        return st.size() == (1 << k);
     }
 };
