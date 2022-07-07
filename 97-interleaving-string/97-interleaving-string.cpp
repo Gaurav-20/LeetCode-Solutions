1489b5883e9e7f68608d69e1d2cbcebb24d96cbd
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2022-07-07 13:00:32 +0530

    Time: 4 ms (73.94%), Space: 6.5 MB (75.79%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
+    string s1, s2, s3;
+    int n1, n2, n3;
+    int dp[101][101];
+    
+    bool solve(int i, int j, int k) {
+        if (i == n1) {
+            return s2.substr(j) == s3.substr(k);
+        }
+        if (j == n2) {
+            return s1.substr(i) == s3.substr(k);
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        bool possible = false;
+        possible |= (s1[i] == s3[k] && solve(i + 1, j, k + 1)) || 
+                    (s2[j] == s3[k] && solve(i, j + 1, k + 1));
+        return dp[i][j] = possible;
+    }
+    
     bool isInterleave(string s1, string s2, string s3) {
-        int n = s1.size(), m = s2.size();
-        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
-        if ((n + m) != s3.size()) {
+        n1 = s1.size(), n2 = s2.size(), n3 = s3.size();
+        this->s1 = s1, this->s2 = s2, this->s3 = s3;
+        if (n1 + n2 != n3) {
             return false;
         }
-        
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
-                if (i == 0 && j == 0) {
-                    dp[i][j] = true;
-                } else if (i == 0) {
-                    if (s2[j - 1] == s3[j - 1]) {
-                        dp[i][j] = dp[i][j - 1];
-                    }
-                } else if (j == 0) {
-                    if (s1[i - 1] == s3[i - 1]) {
-                        dp[i][j] = dp[i - 1][j];
-                    }
-                } else if (s1[i - 1] == s3[i + j - 1] && s2[j - 1] != s3[i + j - 1]) {
-                    dp[i][j] = dp[i - 1][j];
-                } else if (s1[i - 1] != s3[i + j - 1] && s2[j - 1] == s3[i + j - 1]) {
-                    dp[i][j] = dp[i][j - 1];
-                } else if (s1[i - 1] == s3[i + j - 1] && s2[j - 1] == s3[i + j - 1]) {
-                    dp[i][j] = (dp[i - 1][j] || dp[i][j - 1]);
-                }
-            }
-        }
-        return dp[n][m];
+        memset(dp, -1, sizeof(dp));
+        return solve(0, 0, 0);
     }
 };
