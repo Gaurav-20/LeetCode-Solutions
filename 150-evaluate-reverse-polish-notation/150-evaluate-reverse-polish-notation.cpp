999131ed237ea5fb1fce6bc79d9b11e9ce2d9973
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2022-07-12 17:49:41 +0530

    Time: 4 ms (98.70%), Space: 11.9 MB (61.70%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        unordered_map<string, function<int (int, int)>> mp = {
-            { "+" , [] (int a, int b) { return a + b; } },
-            { "-" , [] (int a, int b) { return a - b; } },
-            { "*" , [] (int a, int b) { return a * b; } },
-            { "/" , [] (int a, int b) { return a / b; } }
-        };
         stack<int> st;
-        for (string &s : tokens) {
-            if (!mp.count(s)) {
+        for (string& s : tokens) {
+            if (s.size() > 1 || isdigit(s[0])) {
                 st.push(stoi(s));
             } else {
-                int op1 = st.top();
-                st.pop();
-                int op2 = st.top();
-                st.pop();
-                st.push(mp[s](op2, op1));
+                int x2 = st.top(); st.pop();
+                int x1 = st.top(); st.pop();
+                int res = 0;
+                switch (s[0]) {
+                    case '+': res = x1 + x2; break;
+                    case '-': res = x1 - x2; break;
+                    case '*': res = x1 * x2; break;
+                    case '/': res = x1 / x2; break;
+                }
+                st.push(res);
             }
         }
         return st.top();
