8a78ed163ccc7d9fc227cd50f92b594b21289da2
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2022-08-03 21:01:04 +0530

    Time: 152 ms (60.50%), Space: 38.9 MB (48.99%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,18 +1,18 @@
 class MyCalendar {
 public:
-    set<pair<int, int>> bookings;
+    map<int, int> bookings;
     
     MyCalendar() { }
     
     bool book(int start, int end) {
-        auto next = bookings.lower_bound({ start, end });
+        auto next = bookings.lower_bound(start);
         if (next != bookings.end() && next->first < end) {
             return false;
         }
-        if (next != bookings.begin() && start < (--next)->second) {
+        if (next != bookings.begin() && (--next)->second > start) {
             return false;
         }
-        bookings.insert({ start, end });
+        bookings[start] = end;
         return true;
     }
 };
