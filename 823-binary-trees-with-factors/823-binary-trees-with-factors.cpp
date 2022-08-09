938c648949a8603fa801bfba852535be0f536df2
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2022-08-09 11:53:24 +0530

    Time: 1945 ms (5.23%), Space: 22 MB (5.85%) - LeetHub

---
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -0,0 +1,29 @@
+const int MOD = 1e9 + 7;
+#define mul(a, b) ((a % MOD) * (b % MOD)) % MOD
+
+class Solution {
+public:
+    void solveForRoot(int root, map<int, long long>& dp) {
+        long long count = 1;
+        for (auto it : dp) {
+            if (root % it.first == 0 && dp[root / it.first]) {
+                count = (count + mul(it.second, dp[root / it.first])) % MOD;
+            }
+        }
+        dp[root] = count;
+    }
+    
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        int n = arr.size();
+        map<int, long long> dp;
+        sort(arr.begin(), arr.end());
+        for (auto root : arr) {
+            solveForRoot(root, dp);
+        }
+        long long res = 0;
+        for (auto it : dp) {
+            res = (res + it.second) % MOD;
+        }
+        return (int) res;
+    }
+};
