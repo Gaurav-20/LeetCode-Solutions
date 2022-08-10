18e6365d0e5984a6c0643be54ab82a82872095aa
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2022-08-10 16:59:48 +0530

    Time: 14 ms (11.29%), Space: 7.5 MB (6.77%) - LeetHub

---
+++ 843-guess-the-word/843-guess-the-word.cpp
@@ -0,0 +1,52 @@
+/**
+ * // This is the Master's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class Master {
+ *   public:
+ *     int guess(string word);
+ * };
+ */
+class Solution {
+public:
+    vector<string> reduceWordList (int match, string word, vector<string>& wordlist){
+        vector<string> filterList; 
+        for (string s: wordlist) {
+            int count = 0;
+            for (int i = 0; i < word.size(); i++) {
+                if (word[i] == s[i]) count++;
+            }
+            if (count == match) filterList.push_back(s);
+        }
+        return filterList;
+    }
+    string getCommonWord (vector<string>& wordlist) {
+        string bestWord; 
+        map<int, map<char, int>> wordfreq;
+        int bestScore = 0;
+        for (string s: wordlist) {
+            for(int i = 0; i < s.size(); i++) {
+                wordfreq[i][s[i]]++;
+            }
+        }
+        for (string s: wordlist) {
+            int currentScore = 0;
+            for (int i = 0; i < s.size(); i++) {
+                currentScore += wordfreq[i][s[i]];
+            }
+            if (currentScore > bestScore) {
+                bestScore = currentScore;
+                bestWord = s;
+            }
+        }
+        return bestWord;
+    }
+    
+    void findSecretWord(vector<string>& wordlist, Master& master) {
+        while (!wordlist.empty()) {
+            string guess_word = getCommonWord(wordlist);
+            int match = master.guess(guess_word);
+            if (match == 6) return;
+            wordlist = reduceWordList(match, guess_word, wordlist);
+        }
+    }
+};
