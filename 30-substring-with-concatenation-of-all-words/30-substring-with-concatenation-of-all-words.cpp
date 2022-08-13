6cae63a0f51345908bbdf3ffe9c46ef8321d5362
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2022-08-13 10:46:58 +0530

    Time: 300 ms (65.69%), Space: 24.6 MB (66.39%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        unordered_map<string, int> counts;
+        for (string word : words) {
+            counts[word]++;
+        }
+        int n = s.size(), num = words.size(), len = words[0].size();
+        vector<int> indexes;
+        for (int i = 0; i < n - num * len + 1; i++) {
+            unordered_map<string, int> seen;
+            int j;
+            for (j = 0; j < num; j++) {
+                string word = s.substr(i + j * len, len);
+                if (counts.find(word) != counts.end()) {
+                    seen[word]++;
+                    if (seen[word] > counts[word]) {
+                        break;
+                    }
+                } else { 
+                    break;
+                }
+            }
+            if (j == num) {
+                indexes.push_back(i);
+            }
+        }
+        return indexes;
+    }
+};
