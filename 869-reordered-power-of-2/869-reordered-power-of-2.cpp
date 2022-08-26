dbeedaaaacc41d1ade99606f45272d46685b0bed
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2022-08-26 20:45:03 +0530

    Time: 0 ms (100.00%), Space: 6 MB (68.93%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    bool isPowerOfTwo(int n) {
-        return __builtin_popcount(n) == 1;
-    }
     
     bool reorderedPowerOf2(int n) {
         string str = to_string(n);
         sort(str.begin(), str.end());
-        do {
-            if (str[0] != '0' && isPowerOfTwo(stoi(str))) {
+        for (int i = 0; i < 32; i++) {
+            string powerOfTwo = to_string(1 << i);
+            sort(powerOfTwo.begin(), powerOfTwo.end());
+            if (str == powerOfTwo) {
                 return true;
             }
-        } while (next_permutation(str.begin(), str.end()));
+        }
         return false;
     }
 };
