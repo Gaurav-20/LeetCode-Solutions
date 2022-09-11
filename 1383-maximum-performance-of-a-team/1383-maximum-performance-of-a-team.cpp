bb508a18ae65577b78e7e520f53468ab32717808
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2022-09-11 09:49:01 +0530

    Time: 225 ms (14.96%), Space: 36.9 MB (34.51%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    static bool comp(vector<int>& a, vector<int>& b) {
-        return a[1] > b[1];
+    static bool comp(const pair<int, int>& a, const pair<int, int>& b) {
+        return a.second > b.second;
     }
 
     int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
         priority_queue<int, vector<int>, greater<int>> heap;
-        vector<vector<int>> worker;
+        vector<pair<int, int>> worker;
         for (int i = 0; i < n; i++) {
             worker.push_back({ speed[i], efficiency[i] });
         }
@@ -14,18 +14,18 @@ public:
         long long res = 0, total = 0;
         long minE;
         for (int i = 0; i < k; i++) {
-            total += worker[i][0];
-            minE = worker[i][1];
+            total += worker[i].first;
+            minE = worker[i].second;
             res = max(res, total * minE);
-            heap.push(worker[i][0]);
+            heap.push(worker[i].first);
         }
         for (int i = k; i < n; i++) {
-            if (worker[i][0] > heap.top()) {
-                total += (-heap.top() + worker[i][0]);
-                minE = worker[i][1];
+            if (worker[i].first > heap.top()) {
+                total += (-heap.top() + worker[i].first);
+                minE = worker[i].second;
                 res = max(res, total * minE);
                 heap.pop();
-                heap.push(worker[i][0]);
+                heap.push(worker[i].first);
             }
         }
         return (int)(res % 1000000007);
