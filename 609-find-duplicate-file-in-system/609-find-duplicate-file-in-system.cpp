3440bb825751f6ca6f28966aecd83e9bdf7f3de2
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2022-09-19 12:46:52 +0530

    Time: 232 ms (27.10%), Space: 40.5 MB (54.20%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -1,51 +1,20 @@
-struct Node {
-    string dir = "";
-    vector<string> files;
-};
-
 class Solution {
 public:
-    Node split(string str) {
-        string temp = "";
-        int n = str.size();
-        Node res;
-        for (int i = 0; i < n; i++) {
-            if (str[i] != ' ') {
-                temp += str[i];
-            } else {
-                if (res.dir == "") {
-                    res.dir = temp;
-                } else {
-                    res.files.push_back(temp);
-                }
-                temp = "";
-            }
-        }
-        if (temp != "") {
-            res.files.push_back(temp);
-        }
-        return res;
-    }
     
     vector<vector<string>> findDuplicate(vector<string>& paths) {
         unordered_map<string, vector<string>> mp;
-        for (string path : paths) {
-            Node node = split(path);
-            string dir = node.dir;
-            vector<string> files = node.files;
-            for (auto file : files) {
-                int i;
-                for (i = 0; i < file.size(); i++) {
-                    if (file[i] == '(') {
-                        break;
-                    }
-                }
-                string fileName = file.substr(0, i);
-                string content = file.substr(i);
-                mp[content].push_back(dir + "/" + fileName);
-            }
-        }
-        
+	    for (auto& path : paths) {
+		    stringstream ss(path);
+		    string s, dir, file, content;
+		    ss >> dir;
+		    while (ss >> s) {
+    			auto it = s.find('(');
+    			file = s.substr(0, it);
+    			content = s.substr(it);
+    			mp[content].push_back(dir + "/" + file);
+    		}
+    	}
+
         vector<vector<string>> res;
         for (auto it : mp) {
             if (it.second.size() > 1) {
