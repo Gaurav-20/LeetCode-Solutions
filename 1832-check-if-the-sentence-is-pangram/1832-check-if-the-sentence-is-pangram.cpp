6b8eaa0c96d068178749ed7dd7ce973aff00800a
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2022-09-26 12:23:42 +0530

    Time: 12 ms (5.97%), Space: 6.5 MB (51.50%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        unordered_set<char> st;
+        for (char c : sentence) {
+            st.insert(c);
+        }
+        return st.size() == 26;
+    }
+};
