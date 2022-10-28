8209bb6c0e23cb2df723ab9cabe2290ad5ea4be4
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2022-10-28 23:02:55 +0530

    Time: 21 ms (13.70%), Space: 26.8 MB (34.44%) - LeetHub

---
+++ 0593-valid-square/0593-valid-square.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int dist(vector<int>& a, vector<int>& b) {
+        return (a[0] - b[0]) * (a[0] - b[0]) + (a[1] - b[1]) * (a[1] - b[1]);
+    }
+    
+    bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
+        unordered_set<int> st({
+            dist(p1, p2), dist(p1, p3), dist(p1, p4), dist(p2, p3), dist(p2, p4), dist(p3, p4)
+        });
+        return !st.count(0) && st.size() == 2; // check if its not a single point and there must be two different lengths
+        // four sides and two diagonals
+    }
+};
