5ef7eb80721f0ca162c5d39a732eb349a7b1948a
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2022-10-28 12:39:38 +0530

    Time: 524 ms (36.76%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT DISTINCT email FROM (SELECT ROW_NUMBER()   
-OVER (PARTITION BY email) AS row_num, email
-FROM Person) P
-WHERE row_num > 1;
+SELECT email
+FROM Person
+GROUP BY email
+HAVING COUNT(email) > 1;
