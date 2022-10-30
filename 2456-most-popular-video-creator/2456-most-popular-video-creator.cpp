4f799ad9cd003e4eef95abe6967a32ecb30a4213
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2022-10-30 12:00:00 +0530

    Time: 1475 ms (40.00%), Space: 244.2 MB (10.00%) - LeetHub

---
+++ 2456-most-popular-video-creator/2456-most-popular-video-creator.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<string>> mostPopularCreator(vector<string>& creators, vector<string>& ids, vector<int>& views) {
+        int n = views.size();
+        unordered_map<string, string> creatorToIds;
+        unordered_map<string, long long> creatorToViews;
+        unordered_map<string, long long> creatorToMostViewed;
+        for (int i = 0; i < n; i++) {
+            if (creatorToMostViewed[creators[i]] < views[i]) {
+                creatorToMostViewed[creators[i]] = views[i];
+                creatorToIds[creators[i]] = ids[i];
+            } else if (creatorToMostViewed[creators[i]] == views[i]) {
+                if (creatorToIds.find(creators[i]) != creatorToIds.end()) {
+                    creatorToIds[creators[i]] = min(creatorToIds[creators[i]], ids[i]);
+                } else {
+                    creatorToIds[creators[i]] = ids[i];
+                }
+            }
+            creatorToViews[creators[i]] += views[i];
+        }
+        long long maxViews = -1;
+        for (auto it : creatorToViews) {
+            maxViews = max(maxViews, it.second);
+        }
+        vector<vector<string>> res;
+        for (auto it : creatorToViews) {
+            if (it.second == maxViews) {
+                res.push_back({ it.first, creatorToIds[it.first] });
+            }
+        }
+        return res;
+    }
+};
