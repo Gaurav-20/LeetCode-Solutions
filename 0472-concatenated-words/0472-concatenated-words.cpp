7d3e480f2f55c8d798b8e31924150cc5e8cd4963
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-01-27 12:01:21 +0530

    Time: 432 ms (42.27%), Space: 56.6 MB (46.00%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
+    unordered_set<string> wordSet;
+    unordered_map<string, bool> dp;
+    
+    bool containsWord(string word) {
+        return wordSet.find(word) != wordSet.end();
+    }
+    
+    bool dfs(string word) {
+        if (dp.find(word) != dp.end()) {
+            return dp[word];
+        }
+        for (int i = 1; i < word.size(); i++) {
+            string prefix = word.substr(0, i);
+            string suffix = word.substr(i, word.size() - i + 1);
+            if (containsWord(prefix) && (containsWord(suffix) || dfs(suffix))) {
+                return dp[word] = true;
+            }
+        }
+        return dp[word] = false;
+    }
+    
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
-        unordered_set<string> wordSet;
         for (string word : words) {
             wordSet.insert(word);
         }
         vector<string> res;
         for (string word : words) {
-            int n = word.size();
-            vector<int> dp(n + 1, 0);
-            dp[0] = 1;
-            for (int i = 0; i < n; i++) {
-                if (!dp[i]) {
-                    continue;
-                }
-                for (int j = i + 1; j <= n; j++) {
-                    if (j - i < n && wordSet.count(word.substr(i, j - i))) {
-                        dp[j] = 1;
-                    }
-                }
-                if (dp[n]) {
-                    res.push_back(word);
-                    break;
-                }
+            if (dfs(word)) {
+                res.push_back(word);
             }
         }
         return res;
