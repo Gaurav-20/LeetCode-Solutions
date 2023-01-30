6bd2fbdc7d69a4c25563e9ffb8808bdefe732d85
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-01-30 12:25:57 +0530

    Time: 3 ms (38.83%), Space: 6 MB (37.24%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int tribo[38] = { -1 };
+
+    int tribonacci(int n) {
+        if (tribo[0] == -1) {
+            tribo[0] = 0;
+            tribo[1] = tribo[2] = 1;
+            for (int i = 3; i <= 37; i++) {
+                tribo[i] = tribo[i - 1] + tribo[i - 2] + tribo[i - 3];
+            }
+        }
+        return tribo[n];
+    }
+};
