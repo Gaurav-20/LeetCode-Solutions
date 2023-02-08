11f2746e57e7ac7b57cc7064afb6574f269ce514
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-02-08 19:51:08 +0530

    Time: 16 ms (80.26%), Space: 16.7 MB (56.17%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int len = nums.size() - 1;
+        if (len == 0) {
+            return 0;
+        }
+        int curr = -1, next = 0, ans = 0;
+        for (int i = 0; next < len; i++) {
+            if (i > curr) { 
+                ans++, curr = next;
+            }
+            next = max(next, nums[i] + i);
+        };
+        return ans;
+    }
+};
