f09e4906b22f39054e7794f8a675ae2f350d8aa5
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-03-04 22:41:33 +0530

    Time: 390 ms (50.00%), Space: 89.4 MB (50.00%) - LeetHub

---
+++ 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.cpp
@@ -0,0 +1,43 @@
+const long long MOD = 1e9 + 7;
+
+long long fastpow(long long a, long long b, long long m = MOD) {
+    long long res = 1;
+    a %= m;
+    while (b > 0) {
+        if (b & 1) {
+            res = (res * a) % m;
+        }
+        a = (a * a) % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+class Solution {
+public:
+    int mergedSize(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res;
+        int len = size(intervals);
+        int start = intervals[0][0], end = intervals[0][1];
+
+        for (int i = 1; i < intervals.size(); i++) {
+            if(intervals[i][0] <= end && intervals[i][1] <= end)
+                continue;
+            else if (intervals[i][0] <= end) 
+                end = intervals[i][1];
+            else {
+                res.push_back({ start, end });
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+        res.push_back({start, end});
+        return res.size();
+    }
+    
+    int countWays(vector<vector<int>>& ranges) {
+        long long size = mergedSize(ranges);
+        return fastpow(2LL, size);
+    }
+};
