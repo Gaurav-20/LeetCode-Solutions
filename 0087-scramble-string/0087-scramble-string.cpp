7eb28a68bcde9a98a9e2b47508ce3ae7cb983c2f
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-03-30 23:47:28 +0530

    Time: 178 ms (38.68%), Space: 37.3 MB (31.28%) - LeetHub

---
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    unordered_map<string, bool> mp;
+
+    bool isScramble(string s1, string s2) {
+        if (s1 == s2) {
+            return true;
+        }
+        string key = s1 + "*" + s2;
+        if (mp.count(key)) {
+            return mp[key];
+        }
+        for (int i = 0; i < s1.size() - 1; i++) {
+            if (isScramble(s1.substr(0, i + 1), s2.substr(0, i + 1))
+                && isScramble(s1.substr(i + 1), s2.substr(i + 1))) {
+                return mp[key] = true;
+            }
+            if (isScramble(s1.substr(0, i + 1), s2.substr(s2.size() - i - 1))
+                && isScramble(s1.substr(i + 1), s2.substr(0, s2.size() - i - 1))) {
+                return mp[key] = true;
+            }
+        }
+        return mp[key] = false;
+    }
+};
