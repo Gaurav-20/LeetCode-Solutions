a1067d840e6075fa7e2b4348a8b7721f9409f2f3
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-04-09 14:40:25 +0530

    Time: 347 ms (65.98%), Space: 39.6 MB (12.03%) - LeetHub

--- 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.cpp
+++ 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.cpp
@@ -2,19 +2,19 @@ typedef long long ll;
 
 class Solution {
 public:
-    long long solve(int row, int col, map<pair<int, int>, ll>& mp, vector<vector<ll>>& dp) {
-        if (dp[row][col] != -1) {
-            return dp[row][col];
-        }
-        ll res = mp[{ row, col }];
-        for (int i = 1; i <= row / 2; i++) {
-            res = max(res, solve(i, col, mp, dp) + solve(row - i, col, mp, dp));
-        }
-        for (int i = 1; i <= col / 2; i++) {
-            res = max(res, solve(row, i, mp, dp) + solve(row, col - i, mp, dp));
-        }
-        return dp[row][col] = res;
-    }
+    // long long rec(int row, int col, map<pair<int, int>, ll>& mp, vector<vector<ll>>& dp) {
+    //     if (dp[row][col] != -1) {
+    //         return dp[row][col];
+    //     }
+    //     ll res = mp[{ row, col }];
+    //     for (int i = 1; i <= row / 2; i++) {
+    //         res = max(res, rec(i, col, mp, dp) + rec(row - i, col, mp, dp));
+    //     }
+    //     for (int i = 1; i <= col / 2; i++) {
+    //         res = max(res, rec(row, i, mp, dp) + rec(row, col - i, mp, dp));
+    //     }
+    //     return dp[row][col] = res;
+    // }
     
     long long sellingWood(int m, int n, vector<vector<int>>& prices) {
         map<pair<int, int>, ll> mp;
@@ -22,6 +22,19 @@ public:
             mp[{ it[0], it[1] }] = it[2];
         }
         vector<vector<ll>> dp(m + 1, vector<ll>(n + 1, -1LL));
-        return solve(m, n, mp, dp);
+        // return rec(m, n, mp, dp);
+        for (int row = 0; row <= m; row++) {
+            for (int col = 1; col <= n; col++) {
+                ll res = mp[{ row, col }];
+                for (int i = 1; i <= row / 2; i++) {
+                    res = max(res, dp[i][col] + dp[row - i][col]);
+                }
+                for (int i = 1; i <= col / 2; i++) {
+                    res = max(res, dp[row][i] + dp[row][col - i]);
+                }
+                dp[row][col] = res;
+            }
+        }
+        return dp[m][n];
     }
 };
