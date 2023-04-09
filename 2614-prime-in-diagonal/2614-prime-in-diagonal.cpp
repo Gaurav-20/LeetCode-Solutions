15cb8ed4ca277296ece2f60f9b777bb31d226c22
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-04-09 14:56:15 +0530

    Time: 128 ms (36.36%), Space: 35.7 MB (9.09%) - LeetHub

--- 2614-prime-in-diagonal/2614-prime-in-diagonal.cpp
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.cpp
@@ -4,9 +4,6 @@ public:
         if (n <= 1) {
             return false;
         }
-        if (n <= 3) {
-            return true;
-        }
         for (int i = 2; i * i <= n; i++) {
             if (n % i == 0) {
                 return false;
