925f0042aa3916026f8a5775daf964f28cab8879
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-05-13 11:03:47 +0530

    Time: 21 ms (36.68%), Space: 12.4 MB (59.89%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -3,27 +3,42 @@ const ll MOD = 1e9 + 7;
 
 class Solution {
 public:
-    int low, high, zero, one;
+//     int low, high, zero, one;
     
-    ll solve(int length, vector<ll>& dp) {
-        if (length > high) {
-            return 0;
-        }
-        if (dp[length] != -1) {
-            return dp[length];
-        }
-        if (length >= low && length <= high) {
-            return dp[length] = (1 + solve(length + zero, dp) + solve(length + one, dp)) % MOD;
-        }
-        return dp[length] = (solve(length + zero, dp) + solve(length + one, dp)) % MOD;
-    }
+//     ll solve(int length, vector<ll>& dp) {
+//         if (length > high) {
+//             return 0;
+//         }
+//         if (dp[length] != -1) {
+//             return dp[length];
+//         }
+//         if (length >= low && length <= high) {
+//             return dp[length] = (1 + solve(length + zero, dp) + solve(length + one, dp)) % MOD;
+//         }
+//         return dp[length] = (solve(length + zero, dp) + solve(length + one, dp)) % MOD;
+//     }
     
     int countGoodStrings(int low, int high, int zero, int one) {
-        this->low = low;
-        this->high = high;
-        this->zero = zero;
-        this->one = one;
-        vector<ll> dp(high + 1, -1LL);
-        return solve(0LL, dp);
+        // this->low = low;
+        // this->high = high;
+        // this->zero = zero;
+        // this->one = one;
+        // vector<ll> dp(high + 1, -1LL); // memoization
+        // return solve(0LL, dp);
+        vector<ll> dp(high + 1, 0LL); // dp[i] means number of string of that length
+        dp[0] = 1;
+        for (int i = 1; i <= high; i++) {
+            if (i >= zero) {
+                dp[i] = (dp[i] + dp[i - zero]) % MOD;
+            }
+            if (i >= one) {
+                dp[i] = (dp[i] + dp[i - one]) % MOD;
+            }
+        }
+        ll res = 0;
+        for (int i = low; i <= high; i++) {
+            res = (res + dp[i]) % MOD;
+        }
+        return res;
     }
 };
