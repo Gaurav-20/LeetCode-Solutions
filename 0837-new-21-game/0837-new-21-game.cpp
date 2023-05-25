e9b871e9a12aa7b8bed0cd0a8cf8ea8fc4663fa2
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-05-25 11:12:30 +0530

    Time: 4 ms (68.55%), Space: 11.6 MB (9.68%) - LeetHub

---
+++ 0837-new-21-game/0837-new-21-game.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+        if (n >= k + maxPts - 1) {
+            return 1.0;
+        }
+        
+        double prob = 1.0 / maxPts; // Probability of each score
+        double prev = 0.0;
+        
+        vector<double> dp(n + maxPts);
+        dp[0] = 1.0;
+        
+        for (int i = 1; i <= k; i++) {
+            prev = prev - ((i >= maxPts + 1) ? dp[i - maxPts - 1] : 0.0) + dp[i - 1];
+            dp[i] = prev * prob;
+        }
+        
+        double res = dp[k];
+        
+        for (int i = k + 1; i <= n; i++) {
+            prev = prev - ((i >= maxPts + 1) ? dp[i - maxPts - 1] : 0.0);
+            dp[i] = prev * prob;
+            res += dp[i];
+        }
+        
+        return res;
+    }
+};
