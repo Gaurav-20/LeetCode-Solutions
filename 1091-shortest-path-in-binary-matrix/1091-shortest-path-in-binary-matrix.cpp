518970a98745354bd3be70e3a45b57af8ba1ad9b
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-06-01 12:06:29 +0530

    Time: 52 ms (98.87%), Space: 20.1 MB (72.13%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -10,14 +10,12 @@ vector<vector<int>> directions = {
 
 class Solution {
 public:
-    int n;
-    
-    bool isValidCell(vector<vector<int>>& grid, int x, int y) {
+    bool isValidCell(vector<vector<int>>& grid, int x, int y, int n) {
         return x >= 0 && y >= 0 && x < n && y < n && (grid[x][y] == 0);
     }
     
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        n = grid.size();
+        int n = grid.size();
         if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
             return -1;
         }
@@ -30,10 +28,10 @@ public:
             if (curr.xCoordinate == n - 1 && curr.yCoordinate == n - 1) {
                 return curr.distance;
             }
-            for (auto dir: directions) {
-                int nextXCoordinate = curr.xCoordinate + dir[0];
-                int nextYCoordinate = curr.yCoordinate + dir[1];
-                if (isValidCell(grid, nextXCoordinate, nextYCoordinate)) {
+            for (int i = 0; i < 8; i++) {
+                int nextXCoordinate = curr.xCoordinate + directions[i][0];
+                int nextYCoordinate = curr.yCoordinate + directions[i][1];
+                if (isValidCell(grid, nextXCoordinate, nextYCoordinate, n)) {
                     grid[nextXCoordinate][nextYCoordinate] = -1;
                     q.push({ nextXCoordinate, nextYCoordinate, curr.distance + 1 });
                 }
