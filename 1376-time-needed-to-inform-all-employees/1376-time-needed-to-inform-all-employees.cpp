abbe1aecbfeb42af1fec973498fb10bca3ecaf5b
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-06-03 11:45:36 +0530

    Time: 523 ms (17.90%), Space: 162.3 MB (11.13%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        if (n == 1) {
+            return 0;
+        }
         unordered_map<int, vector<int>> children;
         for (int i = 0; i < n; i++) {
             children[manager[i]].push_back(i);
         }
         queue<pair<int, int>> q;
-        q.push({ headID, 0 });
-        int res = 0;
+        q.push({ headID, 0 }); // node, timeTaken
+        int maxTime = 0;
         while (!q.empty()) {
             int size = q.size();
             while (size--) {
-                auto [head, timeTaken] = q.front();
+                int curr = q.front().first;
+                int time = q.front().second;
                 q.pop();
-                int duration = informTime[head] + timeTaken;
-                res = max(res, duration);
-                for (auto it : children[head]) {
-                    q.push({ it, duration });
+                int duration = time + informTime[curr];
+                maxTime = max(maxTime, duration);
+                for (auto child: children[curr]) {
+                    q.push({ child, duration });
                 }
             }
         }
-        return res;
+        return maxTime;
     }
 };
