51d0d405c0e95108292eb4dfde2082139aa86c0b
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-06-25 16:43:11 +0530

    Time: 279 ms (22.93%), Space: 11.8 MB (42.54%) - LeetHub

---
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
@@ -0,0 +1,25 @@
+const int MOD = 1000000007;
+
+class Solution {
+public:
+    int solve(vector<int>& locations, int city, int finish, int remainFuel, vector<vector<int>>& dp) {
+        if (remainFuel < 0) {
+            return 0;
+        }
+        if (dp[city][remainFuel] != -1) {
+            return dp[city][remainFuel];
+        }
+        int total = (city == finish) ? 1 : 0;
+        for (int nextCity = 0; nextCity < locations.size(); nextCity++) {
+            if (nextCity != city && remainFuel >= abs(locations[nextCity] - locations[city])) {
+                total = (total + solve(locations, nextCity, finish, remainFuel - abs(locations[nextCity] - locations[city]), dp)) % MOD;
+            }
+        }
+        return dp[city][remainFuel] = total;
+    }
+    
+    int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
+        vector<vector<int>> dp(locations.size(), vector<int>(fuel + 1, -1));
+        return solve(locations, start, finish, fuel, dp);
+    }
+};
