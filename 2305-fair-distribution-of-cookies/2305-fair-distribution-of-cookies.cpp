cfcdcb30961ab3d1951dc8caaf769647e3199a73
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-07-01 10:49:29 +0530

    Time: 5 ms (92.54%), Space: 6.8 MB (98.89%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int res = INT_MAX;
+    
+    void solve(int i, vector<int>& cookies, int k, vector<int>& mask) {
+        if (i == cookies.size()) {
+            int ans = INT_MIN;
+            for (int i = 0; i < k; i++) {
+                ans = max(mask[i], ans);
+            }
+            res = min(ans, res);
+            return;
+        }
+        for (int it = 0; it < k; it++) {
+            mask[it] += cookies[i];
+            solve(i + 1, cookies, k, mask);
+            mask[it] -= cookies[i];
+            if (mask[it] == 0) {
+                break;
+            }
+        }
+        return;
+    }
+    
+    int distributeCookies(vector<int>& cookies, int k) {   
+        vector<int> mask(k, 0);
+        solve(0, cookies, k, mask);
+        return res;
+    }
+};
