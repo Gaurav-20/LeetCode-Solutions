c5d554e60492d63ee39bbf8e1791223c1684abbb
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-07-08 12:00:20 +0530

    Time: 218 ms (49.48%), Space: 69.2 MB (29.21%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
@@ -1,21 +1,19 @@
-#define ll long long
 class Solution {
 public:
-    ll putMarbles(vector<int>& weights, int k) {
+    long long putMarbles(vector<int>& weights, int k) {
         int n = weights.size();
-        vector<ll> wts;
+        vector<long long> wts;
         for (int i = 1; i < n; i++) {
             wts.push_back(weights[i - 1] + weights[i]);
         }
         sort(wts.begin(), wts.end());
-        ll mini = 0;
+        long long mini = 0;
         for (int i = 0; i < k - 1; i++) {
             mini += wts[i];
         }
-        reverse(wts.begin(), wts.end());
-        ll maxi = 0;
+        long long maxi = 0;
         for (int i = 0; i < k - 1; i++) {
-            maxi += wts[i];
+            maxi += wts[wts.size() - i - 1];
         }
         return maxi - mini;
     }
