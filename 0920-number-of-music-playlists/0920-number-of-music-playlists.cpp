7dc8384c696e7b909e19f9747196e3ebdabfb97b
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-08-06 15:36:03 +0530

    Time: 4 ms (75.68%), Space: 8.1 MB (46.85%) - LeetHub

---
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.cpp
@@ -0,0 +1,24 @@
+const int MOD = 1e9 + 7;
+
+class Solution {
+public:
+    long long solve(int n, int goal, int k, vector<vector<int>>& dp) {
+        if (n == 0 && goal == 0) {
+            return 1;
+        }
+        if (n == 0 || goal == 0) {
+            return 0;
+        }
+        if (dp[n][goal] != -1) {
+            return dp[n][goal];
+        }
+        long long pick = solve(n - 1, goal - 1, k, dp) * n;
+        long long notPick = solve(n, goal - 1, k, dp) * max(n - k, 0);
+        return dp[n][goal] = (pick + notPick) % MOD;
+    }
+
+    int numMusicPlaylists(int n, int goal, int k) {
+        vector<vector<int>> dp(n + 1, vector<int>(goal + 1, -1));
+        return solve(n, goal, k, dp);
+    }
+};
