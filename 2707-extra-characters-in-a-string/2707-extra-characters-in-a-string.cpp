49a41061f8de490da0bf9648eb385b0b94178577
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-09-02 09:26:34 +0530

    Time: 75 ms (95.61%), Space: 64.6 MB (77.91%) - LeetHub

---
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+        int n = s.size();
+        unordered_map<string, int> mp;
+        for (string& word: dictionary) {
+            mp[word]++;
+        }
+        vector<int> dp(n + 1, 0);
+        for (int index = n - 1; index >= 0; index--) {
+            string curr = "";
+            int minExtra = n;
+            for (int cutIdx = index; cutIdx < n; cutIdx++) {
+                curr.push_back(s[cutIdx]);
+                int currExtra = (mp.count(curr)) ? 0 : curr.size();
+                int nextExtra = dp[cutIdx + 1];
+                int totalExtra = currExtra + nextExtra;
+                minExtra = min(minExtra, totalExtra);
+            }
+            dp[index] = minExtra;
+        }
+        return dp[0];
+    }
+};
