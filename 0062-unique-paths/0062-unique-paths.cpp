324f8e857ed94f71d902409345d0d6bf474579e7
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-09-03 09:32:01 +0530

    Time: 2 ms (44.36%), Space: 6 MB (88.67%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        if (n == 1 || m == 1) {
+            return 1;
+        }
+        long long res = 1;
+        int j = 1;
+        for (int i = m; i <= m + n - 2; i++, j++) {       
+            // Instead of taking factorial, keep on multiply & divide
+            res = (res * i) / j;
+        }
+        return (int) res;
+    }
+};
