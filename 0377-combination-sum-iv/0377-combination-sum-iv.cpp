299a4b2bde8b977d7806ffde88e778eb5695656d
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-09-09 11:53:52 +0530

    Time: 3 ms (50.37%), Space: 6.7 MB (46.24%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,16 @@
+unsigned int dp[1001];
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        memset(dp, 0, sizeof(dp));
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int j = 1; j <= nums.size(); j++) {
+                if (i >= nums[j - 1]) {
+                    dp[i] += dp[i - nums[j - 1]];
+                }
+            }
+        }
+        return (int) dp[target];
+    }
+};
