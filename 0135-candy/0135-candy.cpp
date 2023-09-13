1eaf38f129277524458dae031f61de35c1317ecd
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-09-14 00:53:02 +0530

    Time: 17 ms (62.30%), Space: 18 MB (29.28%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int len = ratings.size();
+        vector<int> minCandies(len, 1);
+        for (int i = 1; i < len; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                minCandies[i] = minCandies[i - 1] + 1;
+            }
+        }
+        for (int i = len - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                minCandies[i] = max(minCandies[i], minCandies[i + 1] + 1);
+            }
+        }
+        return accumulate(minCandies.begin(), minCandies.end(), 0);
+    }
+};
