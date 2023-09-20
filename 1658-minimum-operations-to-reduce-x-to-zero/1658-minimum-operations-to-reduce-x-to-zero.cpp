01b07475499534459944ca1352c2e342a8e88b82
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-09-20 20:17:58 +0530

    Time: 114 ms (87.29%), Space: 98.9 MB (36.20%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
-    int minOperations(vector<int>& nums, int x) {
-        int target = -x;
-        for (auto i : nums) {
-            target += i;
+    int maximalSubarraySizeWithSumValue(vector<int>& nums, long long K) {
+        int i = 0, j = 0, sum = 0;
+        int maxLen = INT_MIN;
+        while (j < nums.size()) {
+            sum += nums[j];
+            if (sum == K) {
+                maxLen = max(maxLen, j - i + 1);
+                j++;
+            } else if (sum < K) {
+                j++;
+            } else if (sum > K) {
+                while (sum > K) {
+                    sum -= nums[i];
+                    i++;
+                }
+                if (sum == K) {
+                    maxLen = max(maxLen, j - i + 1);
+                }
+                j++;
+            }
         }
-        if (target < 0) {
+        return maxLen;
+    }
+    
+    int minOperations(vector<int>& nums, int x) {
+        long long sum = accumulate(nums.begin(), nums.end(), 0LL);
+        long long value = sum - x;
+        if (value < 0) {
             return -1;
         }
-        int j = 0;
-        int sum = 0;
-        int ans = INT_MIN;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            sum += nums[i]; 
-            while (sum > target) {
-                sum -= nums[j++];
-            }
-            if (sum == target) {
-                ans = max(ans, i - j + 1);
-            }
+        if (value == 0) {
+            return nums.size();
         }
-        return ans == INT_MIN ? -1 : n - ans;
+        int maxSubarrayLen = maximalSubarraySizeWithSumValue(nums, value);
+        return (maxSubarrayLen == INT_MIN) ? -1: nums.size() - maxSubarrayLen;
     }
 };
