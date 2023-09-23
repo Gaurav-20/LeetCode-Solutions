4ca053a1ae7961f47a9c8cc49341ac0776340f3d
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-09-23 20:25:00 +0530

    Time: 148 ms (39.15%), Space: 52 MB (31.75%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,15 +1,14 @@
-bool comp(const string& a, const string& b) {
-    return a.size() < b.size();
-}
-
 class Solution {
 public:
+    static bool compare(const string& a, const string& b) {
+        return a.size() < b.size();
+    }
     
     int longestStrChain(vector<string>& words) {
-        sort(words.begin(), words.end(), comp);
+        sort(words.begin(), words.end(), compare);
         unordered_map<string, int> dp;
         int res = 0;
-        for (string word : words) {
+        for (string word: words) {
             for (int i = 0; i < word.size(); i++) {
                 string curr = word.substr(0, i) + word.substr(i + 1);
                 dp[word] = max(dp[word], dp[curr] + 1);
