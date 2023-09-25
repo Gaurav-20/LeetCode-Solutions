b21bbad32662f6f7d6089f575e9bec8637e5d9ea
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-09-26 00:49:33 +0530

    Time: 0 ms (100.00%), Space: 7 MB (16.46%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        vector<int> freq(26, 0);
+        for (char& c: t) {
+            freq[c - 'a']++;
+        }
+        for (char& c: s) {
+            freq[c - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                return i + 'a';
+            }
+        }
+        return ' ';
+    }
+};
