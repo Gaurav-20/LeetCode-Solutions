d3ac9b649df3e660f75fb08922048621b4f0f187
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-10-02 21:59:05 +0530

    Time: 47 ms (63.99%), Space: 48.8 MB (22.63%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int len = nums.size();
-        int lastTrue = len - 1; // The nearest position from i where the value is true
+        int lastTrueIndex = len - 1;
         for (int i = len - 2; i >= 0; i--) {
-            if (nums[i] >= lastTrue - i) {
-                lastTrue = i;
+            if (lastTrueIndex - i <= nums[i]) {
+                lastTrueIndex = i;
             }
         }
-            
-        return lastTrue == 0;
+        return lastTrueIndex == 0;
     }
 };
