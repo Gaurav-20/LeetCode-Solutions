49f00666ae47ec8588c5240695426d3a06390674
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-10-26 22:10:35 +0530

    Time: 0 ms (100.00%), Space: 8 MB (57.83%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int len = nums.size();
-        if (len <= 1) {
-            return nums[0];
+        int n = nums.size();
+        // vector<vector<int>> dp(n, vector<int>(2, -1));
+        // dp[0][0] = 0;
+        // dp[0][1] = nums[0];
+        int prevNotTaken = 0;
+        int prevTaken = nums[0];
+        int currNotTaken = 0;
+        int currTaken = nums[0];
+        for (int i = 1; i < n; i++) {
+            // dp[i][0] = max(dp[i - 1][1], dp[i - 1][0]);
+            // dp[i][1] = dp[i - 1][0] + nums[i];
+            currNotTaken = max(prevTaken, prevNotTaken);
+            currTaken = prevNotTaken + nums[i];
+            prevNotTaken = currNotTaken;
+            prevTaken = currTaken;
         }
-        vector<int> dp(len, 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-        for (int i = 2; i < len; i++) {
-            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
-        }
-        return dp.back();
+        // return max(dp[n - 1][0], dp[n - 1][1]);
+        return max(currTaken, currNotTaken);
     }
 };
