0cbfa46f54e0283d6b20016c57c86813df3bc5b3
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-10-27 23:08:04 +0530

    Time: 31 ms (35.40%), Space: 89.6 MB (64.40%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -7,30 +7,21 @@ public:
             return findMedianSortedArrays(nums2, nums1);
         }
         
-        int n = n1 + n2;
-        int left = (n1 + n2 + 1) / 2;
+        int total = n1 + n2;
+        int half = (n1 + n2 + 1) / 2;
         int low = 0, high = n1;
 
         while (low <= high) {
             int mid1 = (low + high) / 2;
-            int mid2 = left - mid1;
+            int mid2 = half - mid1;
 
-            int l1 = INT_MIN, l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
+            int l1 = (mid1 - 1 >= 0)? nums1[mid1 - 1]: INT_MIN;
+            int l2 = (mid2 - 1 >= 0)? nums2[mid2 - 1]: INT_MIN;
+            int r1 = (mid1 < n1)? nums1[mid1]: INT_MAX;
+            int r2 = (mid2 < n2)? nums2[mid2]: INT_MAX;
 
-            if (mid1 < n1) {
-                r1 = nums1[mid1];
-            }
-            if (mid2 < n2) {
-                r2 = nums2[mid2];
-            }
-            if (mid1 - 1 >= 0) {
-                l1 = nums1[mid1 - 1];
-            }
-            if (mid2 - 1 >= 0) {
-                l2 = nums2[mid2 - 1];
-            }
             if (l1 <= r2 && l2 <= r1) {
-                if (n % 2 == 1) {
+                if (total & 1) {
                     return max(l1, l2);
                 } else {
                     return ((double) (max(l1, l2) + min(r1, r2))) / 2.0;
@@ -41,6 +32,6 @@ public:
                 low = mid1 + 1;
             }
         }
-        return 0;
+        return 0.0;
     }
 };
