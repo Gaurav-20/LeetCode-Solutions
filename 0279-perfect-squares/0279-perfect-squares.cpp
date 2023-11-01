4f3c8a2274f6855e5aca9e5dbe6b3ab76d0669da
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-11-02 00:15:33 +0530

    Time: 5 ms (97.25%), Space: 6.7 MB (93.12%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,12 +1,18 @@
+int cache[10001] = { -1 };
+
 class Solution {
 public:
     int numSquares(int n) {
-        vector<int> cache(n + 1, INT_MAX);
-        cache[0] = 0;
-        cache[1] = 1;
-        for (int num = 2; num <= n; num++) {
-            for (int i = 1; i * i <= num; i++) {
-                cache[num] = min(cache[num], 1 + cache[num - (i * i)]);
+        if (cache[0] == -1) {
+            for (int i = 0; i < 10001; i++) {
+                cache[i] = INT_MAX;
+            }
+            cache[0] = 0;
+            cache[1] = 1;
+            for (int num = 2; num <= 10000; num++) {
+                for (int i = 1; i * i <= num; i++) {
+                    cache[num] = min(cache[num], 1 + cache[num - (i * i)]);
+                }
             }
         }
         return cache[n];
