ffd2457b59c008293fd98a06599febfe2b2e4595
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-11-03 09:26:11 +0530

    Time: 0 ms (100.00%), Space: 8 MB (75.36%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -4,16 +4,12 @@ public:
         vector<string> requiredOperations;
         int len = target.size(), it = 0, curr = 1;
         while (it < len) {
-            if (target[it] == curr) {
+            while (target[it] != curr) {
                 requiredOperations.push_back("Push");
-            } else {
-                while (target[it] != curr) {
-                    requiredOperations.push_back("Push");
-                    requiredOperations.push_back("Pop");
-                    curr++;
-                }
-                requiredOperations.push_back("Push");
+                requiredOperations.push_back("Pop");
+                curr++;
             }
+            requiredOperations.push_back("Push");
             curr++;
             it++;
         }
