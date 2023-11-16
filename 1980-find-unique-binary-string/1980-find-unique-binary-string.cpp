71057d0576cdb79367941a28428a4a40fd572e3a
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-11-16 12:16:51 +0530

    Time: 0 ms (100.00%), Space: 10.4 MB (98.79%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,49 +1,11 @@
 class Solution {
 public:
-    int binaryToInt(string s) {
-        int res = 0;
-        for (int i = s.size() - 1; i >= 0; i--) {
-            res = (res * 2) + (s[i] - '0');
-        }
-        return res;
-    }
-    
-    string intToBinary(int num, int n) {
-        string res = "";
-        while (num > 0) {
-            res += ((num % 2) + '0');
-            num /= 2;
-        }
-        while (res.size() < n) {
-            res += '0';
-        }
-        return res;
-    }
-    
-    vector<int> convertToInteger(vector<string>& nums) {
-        vector<int> res;
-        for (string num: nums) {
-            res.push_back(binaryToInt(num));
-        }
-        return res;
-    }
-    
-    int smallestMissing(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        for (int num: nums) {
-            mp[num]++;
-        }
-        for (int i = 0; ; i++) {
-            if (mp[i] == 0) {
-                return i;
-            }
-        }
-        return -1;
-    }
-    
     string findDifferentBinaryString(vector<string>& nums) {
-        vector<int> integerArr = convertToInteger(nums);
-        int smallestMissingNumber = smallestMissing(integerArr);
-        return intToBinary(smallestMissingNumber, nums[0].size());
+        int index = 0;
+        string res = "";
+        for (string& num: nums) {
+            res += ((num[index++] - '0') ^ 1) + '0';
+        }
+        return res;
     }
 };
