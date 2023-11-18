9b1a7172430d9aa606bc2d30d2d8acf487d7b177
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-11-18 11:37:43 +0530

    Time: 169 ms (95.71%), Space: 116.4 MB (99.29%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
@@ -5,8 +5,8 @@ public:
         sort(nums.begin(), nums.end());
         int n = nums.size();
         for (int i = 0; i < n; i++) {
-            if (i == 0 || (nums[i] != nums[i - 1] && nums[i] != nums[i - 1] + 1)) {
-                if (i == n - 1 || (nums[i] != nums[i + 1] && nums[i] != nums[i + 1] - 1)) {
+            if (i == 0 || nums[i] - nums[i - 1] > 1) {
+                if (i == n - 1 || nums[i] - nums[i + 1] < -1) {
                     res.push_back(nums[i]);
                 }
             }
