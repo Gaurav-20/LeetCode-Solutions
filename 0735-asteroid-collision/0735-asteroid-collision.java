840ab75573cda050ff26e52380810609b23d4c1a
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-12-10 16:43:06 +0530

    Time: 5 ms (82.57%), Space: 45.3 MB (6.43%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,47 +1,16 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        List<Integer> res = new ArrayList<>();
-        for (int asteroid : asteroids) {
-            if (res.size() == 0) {
-                res.add(asteroid);
-            } else if (asteroid < 0 && res.get(res.size() - 1) > 0) {
-                int asteroid1 = res.get(res.size() - 1);
-                int asteroid2 = Math.abs(asteroid);
-                res.remove(res.size() - 1);
-                if (asteroid1 == asteroid2) {
-                    continue;
-                } else if (asteroid1 > asteroid2) {
-                    res.add(asteroid1);
-                } else {
-                    boolean destroyed = false;
-                    while (res.size() > 0 && sign(res.get(res.size() - 1)) != sign(asteroid)) {
-                        if (res.get(res.size() - 1) < asteroid2) {
-                            res.remove(res.size() - 1);
-                        } else if (res.get(res.size() - 1) > asteroid2) {
-                            destroyed = true;
-                            break;
-                        } else {
-                            res.remove(res.size() - 1);
-                            destroyed = true;
-                            break;
-                        }
-                    }
-                    if (!destroyed) {
-                        res.add(asteroid);
-                    }
+        LinkedList<Integer> list = new LinkedList<>(); // use LinkedList to simulate stack
+        for (int i = 0; i < asteroids.length; i++) {
+            int a = asteroids[i];
+            if (a > 0 || list.isEmpty() || list.getLast() < 0) {
+                list.add(a);
+            } else if (list.getLast() <= -a) {
+                if (list.pollLast() < -a) {
+                    i--;
                 }
-            } else {
-                res.add(asteroid);
             }
         }
-        int[] result = new int[res.size()];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = res.get(i);
-        }
-        return result;
-    }
-    
-    public int sign(int number) {
-        return (number < 0) ? -1 : 1;
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
