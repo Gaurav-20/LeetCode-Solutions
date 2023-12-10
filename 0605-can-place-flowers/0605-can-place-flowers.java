3b0df09c1e738b2f7c3a17dd9d01aff905f07489
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-12-10 11:42:42 +0530

    Time: 3 ms (5.78%), Space: 44.6 MB (18.59%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,28 +1,16 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int len = flowerbed.length;
-        if (len == 1) {
-            return (n == 0) || (n == 1 && flowerbed[0] != 1);
+        List<Integer> flowerBed = new ArrayList<>();
+        flowerBed.add(0);
+        for (int i = 0; i < flowerbed.length; i++) {
+            flowerBed.add(flowerbed[i]);
         }
-        if (len < n) {
-            return false;
-        }
-        for (int i = 0; i < len; i++) {
-            if (i == 0) {
-                if (flowerbed[i] + flowerbed[i + 1] == 0) {
-                    n--;
-                    i++;
-                }
-            } else if (i == len - 1) {
-                if (flowerbed[i - 1] + flowerbed[i] == 0) {
-                    n--;
-                    i++;
-                }
-            } else {
-                if (flowerbed[i - 1] + flowerbed[i] + flowerbed[i + 1] == 0) {
-                    n--;
-                    i++;
-                }
+        flowerBed.add(0);
+        int len = flowerBed.size();
+        for (int i = 1; i < len - 1; i++) {
+            if (flowerBed.get(i - 1) + flowerBed.get(i) + flowerBed.get(i + 1) == 0) {
+                n--;
+                i++;
             }
         }
         return n <= 0;
