61f57ef65fdd97a7c0e64cc67a38563b1884e9ac
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-12-18 11:56:53 +0530

    Time: 2 ms (84.24%), Space: 41.7 MB (31.51%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,56 +1,39 @@
 class Solution {
-    public int m;
-    public int n;
-    
     public int orangesRotting(int[][] grid) {
-        m = grid.length;
-        n = grid[0].length;
-        List<List<Integer>> cache = new ArrayList<>();
+        int m = grid.length, n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        int fresh = 0, result = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        
         for (int i = 0; i < m; i++) {
-            List<Integer> list = new ArrayList<>();
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0) {
-                    list.add(Integer.MIN_VALUE); // This will never rot
-                } else if (grid[i][j] == 1) {
-                    list.add(Integer.MAX_VALUE); // This will rot, at some time, we will find out
+                if (grid[i][j] == 1) {
+                    fresh++;
+                } else if (grid[i][j] == 2) {
+                    queue.offer(new int[]{ 0, i, j });
+                    visited[i][j] = true;
                 } else {
-                    list.add(0); // This has been already rotten
-                }
-            }
-            cache.add(list);
-        }
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (cache.get(i).get(j) == 0) {
-                    spreadRottenState(cache, i, j, 0);
+                    visited[i][j] = true;
                 }
             }
         }
         
-        int result = Integer.MIN_VALUE;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                result = Math.max(result, cache.get(i).get(j));
+        int[] dirs = new int[]{ 0, 1, 0, -1, 0 };
+        
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int time = curr[0], x = curr[1], y = curr[2];
+            result = Math.max(result, time);
+            for (int i = 0; i < 4; i++) {
+                int row = x + dirs[i], col = y + dirs[i + 1];
+                if (row < 0 || col < 0 || row >= m || col >= n || visited[row][col] == true) {
+                    continue;
+                }
+                fresh--;
+                queue.offer(new int[]{ time + 1, row, col });
+                visited[row][col] = true;
             }
         }
-        if (result == Integer.MIN_VALUE) {
-            result = 0;
-        }
-        if (result == Integer.MAX_VALUE) {
-            result = -1;
-        }
-        return result;
-    }
-    
-    public void spreadRottenState(List<List<Integer>> cache, int x, int y, int time) {
-        if (x < 0 || y < 0 || x >= m || y >= n || cache.get(x).get(y) < time) {
-            return;
-        }
-        cache.get(x).set(y, Math.min(cache.get(x).get(y), time));
-        spreadRottenState(cache, x - 1, y, time + 1);
-        spreadRottenState(cache, x, y - 1, time + 1);
-        spreadRottenState(cache, x + 1, y, time + 1);
-        spreadRottenState(cache, x, y + 1, time + 1);
+        return (fresh == 0) ? result : -1;
     }
 }
