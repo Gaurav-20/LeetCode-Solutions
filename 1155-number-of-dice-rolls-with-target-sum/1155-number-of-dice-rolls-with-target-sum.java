45729d4ddc31cdc35e4d3221fa5d9d1360d9942c
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2023-12-26 12:04:27 +0530

    Time: 26 ms (13.17%), Space: 43.5 MB (12.81%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,32 +1,15 @@
 class Solution {
-    public int MOD = (int) 1e9 + 7;
-    public int numFaces;
-    public int target;
-    
     public int numRollsToTarget(int n, int k, int target) {
-        this.numFaces = k;
-        this.target = target;
         long[][] dp = new long[n + 1][target + 1];
-        for (int i = 0; i <= n; i++) {
-            Arrays.fill(dp[i], -1);
+        dp[0][0] = 1;
+        int MOD = (int) 1e9 + 7;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= target; j++) {
+                for (int face = 1; face <= k && face <= j; face++) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - face]) % MOD;
+                }
+            }
         }
-        return (int) solve(n, 0, dp);
-    }
-    
-    public long solve(int n, int sum, long[][] dp) {
-        if (n == 0) {
-            return (sum == target) ? 1 : 0;
-        }
-        if (sum < 0 || sum > target) {
-            return 0;
-        }
-        if (dp[n][sum] != -1) {
-            return dp[n][sum];
-        }
-        long result = 0;
-        for (int face = 1; face <= numFaces; face++) {
-            result = (result + solve(n - 1, sum + face, dp)) % MOD;
-        }
-        return dp[n][sum] = result % MOD;
+        return (int) dp[n][target];
     }
 }
