26273a772964f419ae51bed9106d6dfef6b28256
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2024-03-11 17:24:05 +0530

    Time: 24 ms (42.51%), Space: 44.6 MB (77.72%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,40 +1,25 @@
 class Solution {
-    public int[][] dp;
-    
     public boolean isMatch(String str, String pattern) {
-        dp = new int[pattern.length()][str.length()];
-        for (int i = 0; i < pattern.length(); i++) {
-            Arrays.fill(dp[i], -1);
+        int n = pattern.length(), m = str.length();
+        boolean[][] dp = new boolean[n + 1][m + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= n; i++) {
+            if (pattern.charAt(i - 1) != '*') {
+                break;
+            }
+            dp[i][0] = true;
         }
-        return (solve(pattern, str, pattern.length() - 1, str.length() - 1) == 1);
-    }
-    
-    public int solve(String pattern, String str, int i, int j) {
-        if (i < 0 && j < 0) {
-            return 1;
-        }
-        if (i < 0 && j >= 0) {
-            return 0;
-        }
-        if (j < 0 && i >= 0) {
-            for (int k = 0; k <= i; k++) {
-                if (pattern.charAt(k) != '*') {
-                    return 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (pattern.charAt(i - 1) == str.charAt(j - 1) || pattern.charAt(i - 1) == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pattern.charAt(i - 1) == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } else {
+                    dp[i][j] = false;
                 }
             }
-            return 1;
         }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        if (pattern.charAt(i) == str.charAt(j) || pattern.charAt(i) == '?') {
-            return dp[i][j] = solve(pattern, str, i - 1, j - 1);
-        }
-        if (pattern.charAt(i) == '*') {
-            int case1 = solve(pattern, str, i - 1, j);
-            int case2 = solve(pattern, str, i, j - 1);
-            return dp[i][j] = ((case1 == 1) || (case2 == 1)) ? 1 : 0;
-        }
-        return dp[i][j] = 0;
+        return dp[n][m];
     }
 }
