bab979a4a8353aeda339a12dff06e115415cf077
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2024-03-11 17:42:26 +0530

    Time: 25 ms (30.51%), Space: 45.1 MB (46.45%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,25 +1,28 @@
 class Solution {
     public boolean isMatch(String str, String pattern) {
         int n = pattern.length(), m = str.length();
-        boolean[][] dp = new boolean[n + 1][m + 1];
-        dp[0][0] = true;
+        boolean[] curr = new boolean[m + 1];
+        boolean[] prev = new boolean[m + 1];
+        prev[0] = true;
         for (int i = 1; i <= n; i++) {
-            if (pattern.charAt(i - 1) != '*') {
-                break;
-            }
-            dp[i][0] = true;
-        }
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (pattern.charAt(i - 1) == str.charAt(j - 1) || pattern.charAt(i - 1) == '?') {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else if (pattern.charAt(i - 1) == '*') {
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-                } else {
-                    dp[i][j] = false;
+            boolean flag = true;
+            for (int ii = 1; ii <= i; ii++) {
+                if (pattern.charAt(ii - 1) != '*') {
+                    flag = false;
                 }
             }
+            curr[0] = flag;
+            for (int j = 1; j <= m; j++) {
+                if (pattern.charAt(i - 1) == str.charAt(j - 1) || pattern.charAt(i - 1) == '?') {
+                    curr[j] = prev[j - 1];
+                } else if (pattern.charAt(i - 1) == '*') {
+                    curr[j] = prev[j] || curr[j - 1];
+                } else {
+                    curr[j] = false;
+                }
+            }
+            prev = curr.clone();
         }
-        return dp[n][m];
+        return prev[m];
     }
 }
