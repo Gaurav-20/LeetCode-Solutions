5b091a604dcd740e4f566ed0075337919b487116
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2024-03-13 20:18:44 +0530

    Time: 6 ms (88.39%), Space: 44.2 MB (66.25%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,38 +1,56 @@
+class Pair {
+    public int first; // src
+    public int second; // dest
+    
+    Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+
+class Tuple {
+    public int first; // stops
+    public int second; // node
+    public int third; // cost
+    
+    Tuple(int first, int second, int third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+}
+
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        Map<Integer, List<int[]>> adj = new HashMap<>();
-        for (int[] flight : flights) {
-            adj.computeIfAbsent(flight[0], key -> new ArrayList<>()).add(new int[] {flight[1], flight[2]});
+        List<List<Pair>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
         }
-
-        int[] dist = new int[n];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[src] = 0;
-
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {src, 0});
-        int stops = 0;
-
-        while (!q.isEmpty() && stops <= k) {
-            int sz = q.size();
-            while (sz-- > 0) {
-                int[] curr = q.poll();
-                int node = curr[0];
-                int distance = curr[1];
-
-                if (!adj.containsKey(node)) continue;
-
-                for (int[] next : adj.get(node)) {
-                    int neighbour = next[0];
-                    int price = next[1];
-                    if (price + distance >= dist[neighbour]) continue;
-                    dist[neighbour] = price + distance;
-                    q.offer(new int[] {neighbour, dist[neighbour]});
+        for (int i = 0; i < flights.length; i++) {
+            adj.get(flights[i][0]).add(new Pair(flights[i][1], flights[i][2]));
+        }
+        Queue<Tuple> queue = new LinkedList<>();
+        queue.offer(new Tuple(0, src, 0));
+        int[] distance = new int[n];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[src] = 0;
+        while (!queue.isEmpty()) {
+            Tuple tuple = queue.poll();
+            int stops = tuple.first;
+            int node = tuple.second;
+            int cost = tuple.third;
+            if (stops > k) {
+                continue;
+            }
+            for (Pair pair : adj.get(node)) {
+                int neighbor = pair.first;
+                int edgeCost = pair.second;
+                if (cost + edgeCost < distance[neighbor]) {
+                    distance[neighbor] = cost + edgeCost;
+                    queue.offer(new Tuple(stops + 1, neighbor, cost + edgeCost));
                 }
             }
-            stops++;
         }
-
-        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+        return (distance[dst] == Integer.MAX_VALUE) ? -1 : distance[dst];
     }
 }
