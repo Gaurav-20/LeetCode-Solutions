c12794a5d60ad9015c62191cc62bf45b4cbc176d
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2024-03-31 11:10:30 +0530

    Time: 3 ms (100.00%), Space: 55.9 MB (100.00%) - LeetHub

--- 3101-count-alternating-subarrays/3101-count-alternating-subarrays.java
+++ 3101-count-alternating-subarrays/3101-count-alternating-subarrays.java
@@ -1,17 +1,17 @@
 class Solution {
     public long countAlternatingSubarrays(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
+        int start = 0;
+        long result = 0;
         for (int i = 1; i < n; i++) {
-            if (nums[i] != nums[i - 1]) {
-                dp[i] = dp[i - 1] + 1;
+            if (nums[i] == nums[i - 1]) {
+                long len = i - start;
+                result += (len * (len + 1)) / 2;
+                start = i;
             }
         }
-        long result = 0;
-        for (int i = 0; i < n; i++) {
-            result += dp[i];
-        }
+        long len = n - start;
+        result += (len * (len + 1)) / 2;
         return result;
     }
 }
