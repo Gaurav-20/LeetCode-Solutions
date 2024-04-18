b6cf747abeff096579a14410df252e7f7a27abfb
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2024-04-18 11:28:36 +0530

    Time: 5 ms (51.43%), Space: 45.8 MB (31.17%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,33 +1,19 @@
 class Solution {
     public int rows;
     public int cols;
-    public int perimeter;
     
     public int islandPerimeter(int[][] grid) {
         this.rows = grid.length;
         this.cols = grid[0].length;
-        this.perimeter = 0;
-        boolean[][] visited = new boolean[rows][cols];
+        int perimeter = 0;
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                if (!visited[i][j] && grid[i][j] == 1) {
-                    dfs(grid, i, j, visited);
+                if (grid[i][j] == 1) {
+                    perimeter += findSurroundingWaterLength(grid, i, j);
                 }
             }
         }
-        return this.perimeter;
-    }
-    
-    public void dfs(int[][] grid, int x, int y, boolean[][] visited) {
-        if (x < 0 || y < 0 || x >= rows || y >= cols || visited[x][y] || grid[x][y] == 0) {
-            return;
-        }
-        visited[x][y] = true;
-        this.perimeter += findSurroundingWaterLength(grid, x, y);
-        dfs(grid, x + 1, y, visited);
-        dfs(grid, x, y + 1, visited);
-        dfs(grid, x - 1, y, visited);
-        dfs(grid, x, y - 1, visited);
+        return perimeter;
     }
     
     public int findSurroundingWaterLength(int[][] grid, int x, int y) {
