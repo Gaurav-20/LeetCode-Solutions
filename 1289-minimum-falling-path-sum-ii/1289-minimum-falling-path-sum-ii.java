2ad748753179d2abf4e5a3635e0dbbc9f3f6027c
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2024-04-26 20:29:13 +0530

    Time: 30 ms (68.37%), Space: 51.9 MB (7.14%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
@@ -1,26 +1,43 @@
 class Solution {
-    public int n;
-    
     public int minFallingPathSum(int[][] grid) {
-        this.n = grid.length;
-        Integer[][] dp = new Integer[n + 5][n + 5];
-        return solve(grid, 0, -1, dp);
-    }
-
-    public int solve(int[][] grid, int idx, int prevCol, Integer[][] dp) {
-        if (idx == n) {
-            return 0;
+        int n = grid.length;
+        int[][] dp = new int[n][n];
+        // return solve(grid, 0, -1, dp);
+        for (int i = 0; i < n; i++) {
+            dp[n - 1][i] = grid[n - 1][i];
         }
-        if (dp[idx][prevCol + 1] != null) {
-            return dp[idx][prevCol + 1];
-        }
-        int minValue = 1000000;
-        for (int it = 0; it < n; it++) {
-            if (it != prevCol) {
-                int value = grid[idx][it] + solve(grid, idx + 1, it, dp);
-                minValue = Math.min(minValue, value);
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+                int res = 1000000;
+                for (int k = 0; k < n; k++) {
+                    if (k != j) {
+                        res = Math.min(res, dp[i + 1][k]);
+                    }
+                }
+                dp[i][j] = grid[i][j] + res;
             }
         }
-        return dp[idx][prevCol + 1] = minValue;
+        int result = 1000000;
+        for (int i = 0; i < n; i++) {
+            result = Math.min(dp[0][i], result);
+        }
+        return result;
     }
+
+    // public int solve(int[][] grid, int idx, int prevCol, Integer[][] dp) {
+    //     if (idx == n) {
+    //         return 0;
+    //     }
+    //     if (dp[idx][prevCol + 1] != null) {
+    //         return dp[idx][prevCol + 1];
+    //     }
+    //     int minValue = 1000000;
+    //     for (int it = 0; it < n; it++) {
+    //         if (it != prevCol) {
+    //             int value = grid[idx][it] + solve(grid, idx + 1, it, dp);
+    //             minValue = Math.min(minValue, value);
+    //         }
+    //     }
+    //     return dp[idx][prevCol + 1] = minValue;
+    // }
 }
