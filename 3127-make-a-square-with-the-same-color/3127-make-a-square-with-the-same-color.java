448a094065a13773c09a5790a3287c5b6ea55a4a
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2024-04-28 01:19:22 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (50.00%) - LeetHub

--- 3127-make-a-square-with-the-same-color/3127-make-a-square-with-the-same-color.java
+++ 3127-make-a-square-with-the-same-color/3127-make-a-square-with-the-same-color.java
@@ -2,28 +2,20 @@ class Solution {
     public boolean canMakeSquare(char[][] grid) {
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                int whites = 0, blacks = 0;
+                int color = 0;
                 if (grid[i][j] == 'W') {
-                    whites += 1;
-                } else {
-                    blacks += 1;
+                    color += 1;
                 }
                 if (grid[i + 1][j] == 'W') {
-                    whites += 1;
-                } else {
-                    blacks += 1;
+                    color += 1;
                 }
                 if (grid[i + 1][j + 1] == 'W') {
-                    whites += 1;
-                } else {
-                    blacks += 1;
+                    color += 1;
                 }
                 if (grid[i][j + 1] == 'W') {
-                    whites += 1;
-                } else {
-                    blacks += 1;
+                    color += 1;
                 }
-                if (whites >= 3 || blacks >= 3) {
+                if (color != 2) {
                     return true;
                 }
             }
