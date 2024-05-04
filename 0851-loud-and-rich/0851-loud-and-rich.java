0ee89480962b2fee4b4b5ffa183487ea0557c795
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2024-05-05 03:30:22 +0530

    Time: 110 ms (6.05%), Space: 54.9 MB (55.27%) - LeetHub

--- 0851-loud-and-rich/0851-loud-and-rich.java
+++ 0851-loud-and-rich/0851-loud-and-rich.java
@@ -2,34 +2,38 @@ class Solution {
     public int[] loudAndRich(int[][] richer, int[] quiet) {
         int n = quiet.length;
         List<List<Integer>> graph = new ArrayList<>();
-        // Map<Integer, Integer> loudness = new HashMap<>();
         for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
-            // loudness.put(quiet[i], i);
         }
         for (int[] rich : richer) {
             graph.get(rich[1]).add(rich[0]);
         }
         int[] answer = new int[n];
         for (int person = 0; person < n; person++) {
-            // answer[person] = loudness.get(dfs(graph, person, quiet, new boolean[n]));
-            answer[person] = dfs(graph, person, quiet, new boolean[n])[1];
+            answer[person] = bfs(graph, person, quiet, n);
         }
         return answer;
     }
     
-    public int[] dfs(List<List<Integer>> graph, int person, int[] quiet, boolean[] visited) {
+    public int bfs(List<List<Integer>> graph, int person, int[] quiet, int n) {
+        int resultPerson = person, resultLoudness = quiet[person];
+        boolean[] visited = new boolean[n];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(person);
         visited[person] = true;
-        int resultLoudness = quiet[person], resultPerson = person;
-        for (Integer richerPerson : graph.get(person)) {
-            if (!visited[richerPerson]) {
-                int[] currResult = dfs(graph, richerPerson, quiet, visited);
-                if (currResult[0] < resultLoudness) {
-                    resultLoudness = currResult[0];
-                    resultPerson = currResult[1];
+        while (!queue.isEmpty()) {
+            Integer currPerson = queue.poll();
+            if (quiet[currPerson] < resultLoudness) {
+                resultLoudness = quiet[currPerson];
+                resultPerson = currPerson;
+            }
+            for (Integer richerPerson : graph.get(currPerson)) {
+                if (!visited[richerPerson]) {
+                    visited[richerPerson] = true;
+                    queue.offer(richerPerson);
                 }
             }
         }
-        return new int[] { resultLoudness, resultPerson };
+        return resultPerson;
     }
 }
