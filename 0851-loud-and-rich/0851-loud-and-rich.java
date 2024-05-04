0ba9c452f5c88afc0bbd734b7af5da5fb89fa61d
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2024-05-05 03:19:43 +0530

    Time: 108 ms (6.22%), Space: 55.2 MB (34.72%) - LeetHub

--- 0851-loud-and-rich/0851-loud-and-rich.java
+++ 0851-loud-and-rich/0851-loud-and-rich.java
@@ -2,31 +2,34 @@ class Solution {
     public int[] loudAndRich(int[][] richer, int[] quiet) {
         int n = quiet.length;
         List<List<Integer>> graph = new ArrayList<>();
-        Map<Integer, Integer> loudness = new HashMap<>();
+        // Map<Integer, Integer> loudness = new HashMap<>();
         for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
-            loudness.put(quiet[i], i);
+            // loudness.put(quiet[i], i);
         }
         for (int[] rich : richer) {
             graph.get(rich[1]).add(rich[0]);
         }
         int[] answer = new int[n];
         for (int person = 0; person < n; person++) {
-            answer[person] = loudness.get(dfs(graph, person, quiet, new boolean[n]));
+            // answer[person] = loudness.get(dfs(graph, person, quiet, new boolean[n]));
+            answer[person] = dfs(graph, person, quiet, new boolean[n])[1];
         }
         return answer;
     }
     
-    public int dfs(List<List<Integer>> graph, int person, int[] quiet, boolean[] visited) {
+    public int[] dfs(List<List<Integer>> graph, int person, int[] quiet, boolean[] visited) {
         visited[person] = true;
-        int result = quiet[person];
+        int resultLoudness = quiet[person], resultPerson = person;
         for (Integer richerPerson : graph.get(person)) {
             if (!visited[richerPerson]) {
-                result = Math.min(result,
-                                  Math.min(quiet[richerPerson],
-                                           dfs(graph, richerPerson, quiet, visited)));
+                int[] currResult = dfs(graph, richerPerson, quiet, visited);
+                if (currResult[0] < resultLoudness) {
+                    resultLoudness = currResult[0];
+                    resultPerson = currResult[1];
+                }
             }
         }
-        return result;
+        return new int[] { resultLoudness, resultPerson };
     }
 }
