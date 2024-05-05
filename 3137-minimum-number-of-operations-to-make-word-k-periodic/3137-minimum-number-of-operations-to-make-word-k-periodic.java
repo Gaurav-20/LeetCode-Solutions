fd1c51d8d85ea82b726223354b6649e04e6bcd2f
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2024-05-05 10:26:31 +0530

    Time: 31 ms (22.22%), Space: 45.3 MB (66.67%) - LeetHub

--- 3137-minimum-number-of-operations-to-make-word-k-periodic/3137-minimum-number-of-operations-to-make-word-k-periodic.java
+++ 3137-minimum-number-of-operations-to-make-word-k-periodic/3137-minimum-number-of-operations-to-make-word-k-periodic.java
@@ -1,14 +1,14 @@
 class Solution {
     public int minimumOperationsToMakeKPeriodic(String word, int k) {
+        int n = word.length();
         Map<String, Integer> freq = new HashMap<>();
-        for (int i = 0; i < word.length(); i += k) {
+        for (int i = 0; i < n; i += k) {
             String sub = word.substring(i, i + k);
             freq.put(sub, freq.getOrDefault(sub, 0) + 1);
         }
         int result = Integer.MAX_VALUE;
-        int totalSubstrings = word.length() / k;
         for (Integer value : freq.values()) {
-            result = Math.min(result, totalSubstrings - value);
+            result = Math.min(result, n / k - value);
         }
         return result;
     }
