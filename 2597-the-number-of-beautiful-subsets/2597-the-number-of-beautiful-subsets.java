f229679860f97de5013535a5cacafea099118ffc
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2024-05-25 10:41:12 +0530

    Time: 145 ms (78.12%), Space: 44.7 MB (69.92%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.java
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.java
@@ -3,23 +3,23 @@ class Solution {
     
     public int beautifulSubsets(int[] nums, int k) {
         result = 0;
-        solve(nums, 0, k, new ArrayList<>(), new HashMap<>());
+        solve(nums, 0, k, new ArrayList<>(), new int[3005]);
         return result - 1;
     }
     
-    public void solve(int[] nums, int i, int k, List<Integer> list, Map<Integer, Integer> map) {
+    public void solve(int[] nums, int i, int k, List<Integer> list, int[] map) {
         result += 1;
         for (int idx = i; idx < nums.length; idx++) {
-            if (map.containsKey(nums[idx] + k) && map.get(nums[idx] + k) > 0) {
+            if (map[nums[idx] + k + 1000] > 0) {
                 continue;
             }
-            if (map.containsKey(nums[idx] - k) && map.get(nums[idx] - k) > 0) {
+            if (map[nums[idx] - k + 1000] > 0) {
                 continue;
             }
-            map.put(nums[idx], map.getOrDefault(nums[idx], 0) + 1);
+            map[nums[idx] + 1000] += 1;
             list.add(nums[idx]);
             solve(nums, idx + 1, k, list, map);
-            map.put(nums[idx], map.get(nums[idx]) - 1);
+            map[nums[idx] + 1000] -= 1;
             list.remove(list.size() - 1);
         }
     }
