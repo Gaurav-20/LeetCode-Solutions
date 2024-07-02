5591550e179f544df5ad3929fbc02a9e41884979
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2024-07-02 23:47:11 +0530

    Time: 314 ms (81.47%), Space: 0B (100.00%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-SELECT  patient_id, patient_name, conditions
+SELECT *
 FROM Patients
-WHERE conditions LIKE '% DIAB1%'
-OR conditions LIKE 'DIAB1%';
+WHERE conditions LIKE '% DIAB1%' || conditions LIKE 'DIAB1%';
