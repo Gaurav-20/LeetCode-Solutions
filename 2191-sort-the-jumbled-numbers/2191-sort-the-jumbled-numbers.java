d9d2e59074c7e8a58ddfcaa4808dc81468f31fca
Gaurav Sawant <39230629+Gaurav-20@users.noreply.github.com>
2024-07-24 09:28:26 +0530

    Time: 362 ms (28.43%), Space: 55.6 MB (41.18%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.java
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.java
@@ -2,15 +2,15 @@ class Solution {
     public int[] sortJumbled(int[] mapping, int[] nums) {
         int n = nums.length;
         Integer[] arr = new Integer[n];
+        Map<Integer, Integer> mapped = new HashMap<>();
         for (int i = 0; i < n; i++) {
             arr[i] = nums[i];
+            mapped.put(arr[i], getMappedValue(arr[i], mapping));
         }
         Arrays.sort(arr, new Comparator<Integer>() {
             @Override
             public int compare(Integer a, Integer b) {
-                Integer aMapped = getMappedValue(a, mapping);
-                Integer bMapped = getMappedValue(b, mapping);
-                return aMapped - bMapped;
+                return mapped.get(a) - mapped.get(b);
             }
         });
         return Arrays.stream(arr).mapToInt(Integer::intValue).toArray();
